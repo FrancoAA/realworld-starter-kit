528946fc4ec5922b0d394a1ca5fb42b1ac76faab
Franco Alarcon <franco.alarcon@attackiq.com>
2019-11-14 23:16:56 -0300

    minor tweak for the section controls

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -15,13 +15,13 @@ import {
   IonHeader,
   IonSkeletonText,
   IonSpinner
-} from '@ionic/react';
+} from "@ionic/react";
 
-import React, { useEffect, useState } from 'react';
-import { book, build, colorFill, grid } from 'ionicons/icons';
-import { ArticlesService } from '../common/api.service';
+import React, { useEffect, useState } from "react";
+import { book, build, colorFill, grid } from "ionicons/icons";
+import { ArticlesService } from "../common/api.service";
 
-import './Home.scss';
+import "./Home.scss";
 
 function usePaginator(pageSize) {
   const [paginator, setPaginator] = useState({ offset: 0, limit: pageSize });
@@ -29,68 +29,71 @@ function usePaginator(pageSize) {
   const nextPage = () => {
     setPaginator(prev => ({
       offset: prev.offset + prev.limit,
-      limit : prev.limit
+      limit: prev.limit
     }));
-  }
+  };
 
-  return [
-    paginator,
-    nextPage
-  ];
+  return [paginator, nextPage];
 }
 
 const ListSkeleton = ({ items }) => {
   const placeholderItems = [];
 
-  for (let i=0; i<items; i++) {
+  for (let i = 0; i < items; i++) {
     placeholderItems.push(
       <IonItem key={i}>
         <IonAvatar slot="start">
           <IonSkeletonText animated />
         </IonAvatar>
         <IonLabel>
-          <h2><IonSkeletonText animated style={{ width: '50%' }} /></h2>
-          <h3><IonSkeletonText animated style={{ width: '80%' }} /></h3>
-          <p><IonSkeletonText animated style={{ width: '70%' }} /></p>
+          <h2>
+            <IonSkeletonText animated style={{ width: "50%" }} />
+          </h2>
+          <h3>
+            <IonSkeletonText animated style={{ width: "80%" }} />
+          </h3>
+          <p>
+            <IonSkeletonText animated style={{ width: "70%" }} />
+          </p>
         </IonLabel>
       </IonItem>
     );
   }
 
   return placeholderItems;
-}
+};
 
 const Home = () => {
-  const [ articles, setArticles ] = useState([]);
-  const [ loading, setLoading ] = useState(false);
-  const [ paginator, nextPage] = usePaginator(10);
-
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [paginator, nextPage] = usePaginator(10);
+  const [section, setSection] = useState("global");
 
   useEffect(() => {
     async function fetchArticles() {
       setLoading(true);
-      const { data } = await ArticlesService.query('', paginator);
+      const { data } = await ArticlesService.query("", paginator);
       setArticles([...articles, ...data.articles]);
       setLoading(false);
     }
     fetchArticles();
-  }, [paginator])
+  }, [paginator]);
 
   return (
     <IonPage className="Home">
       <IonHeader>
         <IonToolbar color="light">
-          <IonTitle>
-            Conduit
-          </IonTitle>
+          <IonTitle>Conduit</IonTitle>
         </IonToolbar>
         <IonToolbar color="light">
-          <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)}>
-            <IonSegmentButton value="friends">
-              <IonLabel>Your Feed</IonLabel>
-            </IonSegmentButton>
-            <IonSegmentButton value="enemies">
+          <IonSegment
+            onIonChange={e => console.log("Segment selected", e.detail.value)}
+          >
+            <IonSegmentButton value="global" checked={section === "global"}>
               <IonLabel>Global Feed</IonLabel>
+            </IonSegmentButton>
+            <IonSegmentButton value="personal" checked={section === "personal"}>
+              <IonLabel>Your Feed</IonLabel>
             </IonSegmentButton>
           </IonSegment>
         </IonToolbar>
@@ -99,24 +102,29 @@ const Home = () => {
         <IonList lines="full">
           {loading && <ListSkeleton items={5} />}
 
-          {!loading && articles.map(article => (
-            <IonItem key={article.slug} routerLink={`/home/${article.slug}`}>
-              <IonAvatar slot="start">
-                <img src={article.author.image}/>
-              </IonAvatar>
-              <IonLabel>
-                <h2>{article.author.username}</h2>
-                <h3>{article.title}</h3>
-                <p>{article.description}</p>
-              </IonLabel>
-            </IonItem>
-          ))}
+          {!loading &&
+            articles.map(article => (
+              <IonItem key={article.slug} routerLink={`/home/${article.slug}`}>
+                <IonAvatar slot="start">
+                  <img src={article.author.image} />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>{article.author.username}</h2>
+                  <h3>{article.title}</h3>
+                  <p>{article.description}</p>
+                </IonLabel>
+              </IonItem>
+            ))}
         </IonList>
 
-        <IonButton expand="full" color="primary" fill="clear" onClick={nextPage}>
+        <IonButton
+          expand="full"
+          color="primary"
+          fill="clear"
+          onClick={nextPage}
+        >
           Load more...
         </IonButton>
-
       </IonContent>
     </IonPage>
   );
