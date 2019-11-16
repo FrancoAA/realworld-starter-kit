63b665e9e04d2bcdefb46cf39b5ababb8080317b
Franco Alarcon <franco.alarcon@attackiq.com>
2019-11-15 21:03:13 -0300

    Completes the Compose modal UI

--- app/src/common/ComposeModal.jsx
+++ app/src/common/ComposeModal.jsx
@@ -2,14 +2,60 @@ import React from 'react';
 
 import {
   IonModal,
-  IonButton
+  IonButton,
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonLabel,
+  IonInput,
+  IonList,
+  IonTextarea,
+  IonItem,
+  IonFooter
 } from '@ionic/react';
+
+// import { book, build, colorFill, grid, funnel } from "ionicons/icons";
 
 const ComposeModal = ({ isOpen, closeModal}) => {
   return (
     <IonModal isOpen={isOpen}>
-      <p>This is modal content</p>
-      <IonButton onClick={closeModal}>Close Modal</IonButton>
+      <IonHeader>
+        <IonToolbar>
+        <IonButtons slot="start">
+            <IonButton onClick={closeModal}>
+              Cancel
+            </IonButton>
+          </IonButtons>
+          <IonTitle>Compose</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonList lines="full" class="ion-no-margin ion-no-padding">
+          <IonItem>
+            <IonLabel position="stacked">Title</IonLabel>
+            <IonInput></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Description</IonLabel>
+            <IonInput></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Content</IonLabel>
+            <IonTextarea></IonTextarea>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Tags</IonLabel>
+            <IonInput></IonInput>
+          </IonItem>
+        </IonList>
+      </IonContent>
+      <IonFooter>
+        <IonButton expand="full" onClick={closeModal}>
+          Publish
+        </IonButton>
+      </IonFooter>
     </IonModal>
   );
 };
