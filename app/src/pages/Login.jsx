aa917569679922bb1afae828f968ff2b7295f3f9
Franco Alarcon <franco.alarcon@attackiq.com>
2019-11-18 15:12:03 -0300

    - Login page scaffold

---
+++ app/src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import {
+  IonHeader,
+  IonToolbar,
+  IonPage,
+  IonTitle,
+  IonContent
+} from "@ionic/react";
+
+const Login = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent></IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
